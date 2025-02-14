f761460cde6ecaef80fad29880d0ffbfd04ac811
Mike Chen <yi.chen.it@gmail.com>
2025-02-14 13:25:16 -0800

    remove repack build

--- build_and_release.sh
+++ build_and_release.sh
@@ -10,10 +10,6 @@ function do_build {
     echo "=== Building ${version} ==="
     echo
 
-    ./gradlew clean :repacked-platforms:install -Pversion="${version}.0" -PpgVersion="${version}"
-    cp repacked-platforms/build/tmp/buildAmd64DarwinBundle/bundle/postgres-darwin-x86_64.txz "release/postgresql-${version}-darwin-amd64.txz"
-    cp repacked-platforms/build/tmp/buildArm64v8DarwinBundle/bundle/postgres-darwin-arm_64.txz "release/postgresql-${version}-darwin-arm64.txz"
-
     ./gradlew clean install -Pversion="${version}.0" -PpgVersion="${version}" -ParchName=amd64 -PpostgisVersion=3.4.0
     cp custom-debian-platform/build/tmp/buildCustomDebianBundle/bundle/postgres-linux-debian.txz "release/postgresql-${version}-linux-amd64.txz"
 }
