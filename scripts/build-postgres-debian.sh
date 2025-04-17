6a72896b9ef8c9d6a2ac2dbe4a8a4ab9dbdba8e4
Jeffery Chen <jeffery.chen@cadencerpm.com>
2025-04-16 22:07:58 -0400

    modify debian build script to also build pgvector

--- scripts/build-postgres-debian.sh
+++ scripts/build-postgres-debian.sh
@@ -7,14 +7,16 @@ GEOS_VERSION=3.8.3
 GDAL_VERSION=3.4.3
 POSTGIS_VERSION=
 PGROUTING_VERSION=
+PGVECTOR_VERSION=
 LITE_OPT=false
 
-while getopts "v:i:g:r:o:l" opt; do
+while getopts "v:i:g:r:e:o:l" opt; do
     case $opt in
     v) PG_VERSION=$OPTARG ;;
     i) IMG_NAME=$OPTARG ;;
     g) POSTGIS_VERSION=$OPTARG ;;
     r) PGROUTING_VERSION=$OPTARG ;;
+    e) PGVECTOR_VERSION=$OPTARG ;;
     o) DOCKER_OPTS=$OPTARG ;;
     l) LITE_OPT=true ;;
     \?) exit 1 ;;
@@ -36,7 +38,7 @@ ICU_ENABLED=$(echo "$PG_VERSION" | grep -qv '^9\.' && [ "$LITE_OPT" != true ] &&
 TRG_DIR=$PWD/bundle
 mkdir -p $TRG_DIR
 
-docker run -i --rm -v ${TRG_DIR}:/usr/local/pg-dist \
+docker run --platform=linux/amd64 -i --rm -v ${TRG_DIR}:/usr/local/pg-dist \
 -v $PWD/../../../../share:/tmp/share \
 -e PG_VERSION=$PG_VERSION \
 -e POSTGIS_VERSION=$POSTGIS_VERSION \
@@ -46,12 +48,15 @@ docker run -i --rm -v ${TRG_DIR}:/usr/local/pg-dist \
 -e GEOS_VERSION=$GEOS_VERSION \
 -e GDAL_VERSION=$GDAL_VERSION \
 -e PGROUTING_VERSION=$PGROUTING_VERSION \
+-e PGVECTOR_VERSION=0.8.0 \
 $DOCKER_OPTS $IMG_NAME /bin/bash -ex -c 'echo "Starting building postgres binaries" \
     && sed "s@archive.ubuntu.com@us.archive.ubuntu.com@" -i /etc/apt/sources.list \
+    && sed -i "/bionic-backports/d" /etc/apt/sources.list \
     && ln -snf /usr/share/zoneinfo/Etc/UTC /etc/localtime && echo "Etc/UTC" > /etc/timezone \
-    && apt-get update && apt-get install -y --no-install-recommends \
-        ca-certificates \
+    && apt-get update \
+    && apt-get install -y --no-install-recommends \
         wget \
+        patchelf \
         rsync \
         bzip2 \
         xz-utils \
@@ -70,6 +75,9 @@ $DOCKER_OPTS $IMG_NAME /bin/bash -ex -c 'echo "Starting building postgres binari
         python3-dev \
         tcl-dev \
         \
+    && mkdir -p /etc/ssl/certs \
+    && wget --no-check-certificate -O /etc/ssl/certs/ca-certificates.crt https://curl.se/ca/cacert.pem \
+    && export SSL_CERT_FILE=/etc/ssl/certs/ca-certificates.crt \
     && wget -O patchelf.tar.gz "https://nixos.org/releases/patchelf/patchelf-0.9/patchelf-0.9.tar.gz" \
     && mkdir -p /usr/src/patchelf \
     && tar -xf patchelf.tar.gz -C /usr/src/patchelf --strip-components 1 \
@@ -101,8 +109,8 @@ $DOCKER_OPTS $IMG_NAME /bin/bash -ex -c 'echo "Starting building postgres binari
         --with-python \
         --with-tcl \
         --without-readline \
-    && make -j$(nproc) world \
-    && make install-world \
+    && make -j2 \
+    && make install \
     && make -C contrib install \
     \
     && if [ -n "$POSTGIS_VERSION" ]; then \
@@ -155,11 +163,18 @@ $DOCKER_OPTS $IMG_NAME /bin/bash -ex -c 'echo "Starting building postgres binari
           && make \
           && make install \
     ; fi \
+    && if [ -n "$PGVECTOR_VERSION" ]; then \
+      mkdir -p /usr/src/pgvector \
+        && wget -O "https://github.com/pgvector/pgvector/archive/v$PGVECTOR_VERSION.tar.gz" | tar -xzf - -C /usr/src/pgvector --strip-components 1 \
+        && cd /usr/src/pgvector \
+        && make USE_PGXS=1 PG_CONFIG=/usr/local/pg-build/bin/pg_config \
+        && make USE_PGXS=1 PG_CONFIG=/usr/local/pg-build/bin/pg_config install \
+    ; fi \
     \
     && cd /usr/local/pg-build \
-    && cp /usr/lib/libossp-uuid.so.16 ./lib || cp /usr/lib/*/libossp-uuid.so.16 ./lib \
+    && cp /usr/lib/x86_64-linux-gnu/libossp-uuid.so.16 ./lib \
     && cp /lib/*/libz.so.1 /lib/*/liblzma.so.5 /usr/lib/*/libxml2.so.2 /usr/lib/*/libxslt.so.1 ./lib \
-    && cp /lib/*/libssl.so.1* /lib/*/libcrypto.so.1* ./lib || cp /usr/lib/*/libssl.so.1* /usr/lib/*/libcrypto.so.1* ./lib \
+    && cp /usr/lib/x86_64-linux-gnu/libssl.so.1.1 /usr/lib/x86_64-linux-gnu/libcrypto.so.1.1 ./lib \
     && if [ "$ICU_ENABLED" = true ]; then cp --no-dereference /usr/lib/*/libicudata.so* /usr/lib/*/libicuuc.so* /usr/lib/*/libicui18n.so* ./lib; fi \
     && if [ -n "$POSTGIS_VERSION" ]; then cp --no-dereference /lib/*/libjson-c.so* /usr/lib/*/libsqlite3.so* ./lib ; fi \
     && find ./bin -type f \( -name "initdb" -o -name "pg_ctl" -o -name "postgres" -o -name "pg_dump" -o -name "pg_dumpall" -o -name "pg_restore" -o -name "pg_isready" -o -name "psql" \) -print0 | xargs -0 -n1 patchelf --set-rpath "\$ORIGIN/../lib" \
